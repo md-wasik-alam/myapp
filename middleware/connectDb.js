3474ea1dfc24984eef4c83dc59155c6a9e3833dd
md-wasik-alam <wasikalam02@gmail.com>
2023-05-06 04:56:45 +0530

    error solve

--- middleware/connectDb.js
+++ middleware/connectDb.js
@@ -4,7 +4,7 @@ const connectDb = handler => async (req,res) => {
     if(mongoose.connection.readyState){
         return handler(req, res);
     }
-    await mongoose.connect("mongodb://localhost:27017/")
+    await mongoose.connect(process.env.MONGO_URI)
     return handler(req,res);
 }
 
